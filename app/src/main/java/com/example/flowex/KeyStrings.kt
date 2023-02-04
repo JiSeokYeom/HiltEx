550f18bcbb696d92c5a565c0ec2da70e6dab1f5d
JiSeokYeom <ygs01033@naver.com>
2023-02-04 21:40:09 +0900

    feat: API_KEY 숨기기

--- app/src/main/java/com/example/flowex/KeyStrings.kt
+++ app/src/main/java/com/example/flowex/KeyStrings.kt
@@ -1,6 +1,5 @@
 package com.example.flowex
 
 object KeyStrings {
-    const val API_KEY = "n03GwgG26aAn1K98nXITnigQMU1VS6WL"
     const val BASE_URL = "https://api.giphy.com/"
 }
--- app/src/main/java/com/example/flowex/service/GiphyService.kt
+++ app/src/main/java/com/example/flowex/service/GiphyService.kt
@@ -1,6 +1,6 @@
 package com.example.flowex.service
 
-import com.example.flowex.KeyStrings.API_KEY
+import com.example.flowex.BuildConfig
 import com.example.flowex.data.Data
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,6 +8,6 @@ import retrofit2.http.Query
 interface GiphyService {
     @GET("/v1/gifs/trending")
     suspend fun getTrendingGifs(
-        @Query("api_key") apiKey : String = API_KEY,
+        @Query("api_key") apiKey : String = BuildConfig.API_KEY,
     ): Data
 }
